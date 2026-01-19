821e16ce47f45b81fa8a67777991a69081b1aab1
GODARC0 <mohdluqmaanishere@gmail.com>
2026-01-19 21:33:29 +0530

    adding comments
    separating different codes via comments.

--- C++/DAY 2/conditionals.cpp
+++ C++/DAY 2/conditionals.cpp
@@ -2,44 +2,54 @@
 using namespace std;
 
 int main () {
-    // int n = -34;
-    // if (n >= 0){
-    //     cout << "n is positive"<<"\n";
-    // }
-    // else{
-    //     cout<< "n is negative"<<"\n";
-    // }
+    //first condition code
 
-        // int age;
-        // cout<<"enter your age:";
-        // cin>> age;
+    int n = -34;
+    if (n >= 0){
+        cout << "n is positive"<<"\n";
+    }
+    else{
+        cout<< "n is negative"<<"\n";
+    }
 
-        // if (age>>18){
-        //     cout<<"yes you can vote "<<"\n";
-        // }
-        // else{
-        //     cout<<"no you can not vote"<<"\n";
-        // }
 
-        // int num;
-        // cout<<"kitne number ayein hai? ";
-        // cin>>num;
+    //can you vote??
+    
+    int age;
+        cout<<"enter your age:";
+        cin>> age;
+
+        if (age>>18){
+            cout<<"yes you can vote "<<"\n";
+        }
+        else{
+            cout<<"no you can not vote"<<"\n";
+        }
+
+
+
+        //3 conditions, marks batao
+        int num;
+        cout<<"kitne number ayein hai? ";
+        cin>>num;
         
-        // if (num >=90){
-        //     cout<<"shabbash.";
-        // }
-        // else if (num >=80 && num < 90){
-        //     cout<<"aur mehnat karo .";
-        // }
-        // else{
-        //     cout<<"aaj khana nahi milega.";
-        // }
+        if (num >=90){
+            cout<<"shabbash.";
+        }
+        else if (num >=80 && num < 90){
+            cout<<"aur mehnat karo .";
+        }
+        else{
+            cout<<"aaj khana nahi milega.";
+        }
 
+        
+        //identify upper and lower case
         char ch;
         cout<<"enter a character: ";
         cin>>ch;
 
-        if(ch>='a'  && ch<='z' ){                    // we can also use ascaii value of  these jise implicit type conversion kahenge
+        if(ch>='a'  && ch<='z' ){ // we can also use ascaii value of  these jise implicit type conversion kahenge
             cout<<"character is in lowercase"<<"\n";
 
         }
