853d780c9a34e6ef34daf1573d62aed48c5930cf
GODARC0 <mohdluqmaanishere@gmail.com>
2026-01-19 21:43:37 +0530

    separating different codes

--- C++/DAY 2/loops.cpp
+++ C++/DAY 2/loops.cpp
@@ -3,94 +3,98 @@ using namespace std;
 
 
 int main(){
-    // int i=0;
-    // int n;
-    // cout<<"give a number: ";
-    // cin>>n;
-    // int sum =0;
-    // while (count <=n)
-    // {
-    //     cout<<count<<" ";
-    //     count++;
+// sum   
+    int i=0;
+    int n;
+    int count;
+    cout<<"give a number: ";
+    cin>>n;
+    int sum =0;
+    while (count <=n)
+    {
+        cout<<count<<" ";
+        count++;
 
-    // }
-    // cout<<"\n";
-    // for (int i = 1; i <= n; i++)
-    // {
-    //      sum = sum+i;
-    //     if (i>=10){
-    //         break;
-    //     }
-        
-    // }
-    // cout<<"\n";
-    // cout<<"sum is :"<<sum;
+    }
+    cout<<"\n";
 
-    // while( i <= n){
-    //     break;  //turent loop se bahar
-    //     sum=sum+i;
-    //     i++;
+
+
+    for (int i = 1; i <= n; i++)
+    {
+         sum = sum+i;
+        if (i>=10){
+            break;
+        }
         
-    // }
+    }
+    cout<<"\n";
+    cout<<"sum is :"<<sum;
+
+    while( i <= n){
+        break;  //turent loop se bahar
+        sum=sum+i;
+        i++;
+        
+    }
     
 
-    // for (int i = 0; i <= n ; i++){
-    //     if (i%2!=0)
-    //     {
-    //         // cout<< i<<"+";
-    //         sum += i;
-    //     }
-        
-    // }
-    // while (i<=n){
-    //     if (i%2!=0)
-    //     {
-    //         sum = sum+i;
-            
-    //     }
-    //     i++;
-    // }
-    // cout<<"\n";
-    // cout<<"sum of odd no. is :"<<sum;
-    //  string a;
-    // do
-    // {
-       
-    //     cout<<"give me 5: ";
-    //     cin>>a;
-    // } while (a != "5");
 
+
+    for (int i = 0; i <= n ; i++){
+        if (i%2!=0)
+        {
+            // cout<< i<<"+";
+            sum += i;
+        }
+        
+    }
+
+
+
+    while (i<=n){
+        if (i%2!=0)
+        {
+            sum = sum+i;
+            
+        }
+        i++;
+    }
+    cout<<"\n";
+    cout<<"sum of odd no. is :"<<sum;
+
+
+
+
+//prompting again and again until getting right input
+     string a;
+    do
+    {
+       
+        cout<<"give me 5: ";
+        cin>>a;
+    } while (a != "5");
+
+    //identifying prime
     int prime;
     cout<<"enter a number: ";
     cin>>prime;
-    // for(int i = 2 ; i<prime ; i++){
-    //     if(prime%i==0){
-    //         cout<<prime<<" is not a prime number";
-            //// ye logic galat hai kyuki itteration thik se nahi ho rahi to badhi values check  nahi ho pa rahi..
-    //     }
-    //     else{
-    //         cout<<prime<<" is a prime number.";
+// ye logic galat hai kyuki itteration thik se nahi ho rahi to badhi values check  nahi ho pa rahi..
+    for(int i = 2 ; i<prime ; i++){
+        if(prime%i==0){
+            cout<<prime<<" is not a prime number";
+
+        }
+        else{
+            cout<<prime<<" is a prime number.";
             
-    //     }
-    //     return 0;
-    // }
+        }
+        return 0;
+    }
 
 
-    // bool isPrime = true;
-    // for (int i = 2; i < prime; i++)
-    // {
-    //     if(prime % i == 0){
-    //         isPrime = false;
-    //         break;
-    //     }
-    // }
-    // if (isPrime = true){
-    //     cout<<"given is prime number";
-    // }
-    // else{
-    //     cout<<"given is not a prime number";
-    // }
-    // return 0;
+
+    //this method is much better
 
 
      bool isPrime = true;
