80c93467f25c2212d98eb4ffc4eb4fec62878875
GODARC0 <mohdluqmaanishere@gmail.com>
2026-03-05 09:35:28 +0530

    writing pseudo code for initial functions

--- C programming/AtmCopy.c
+++ C programming/AtmCopy.c
@@ -2,5 +2,29 @@
 
 
 int main(void){
-    
+    //this is just a psudo code
+
+    // promting -> insert card details (6 digit int number)
+    // promt -> select language (hindi , english)
+    // promt -> a; manage debit card (will work on already existing database) b; enter atm pin c;set/generate ATM pin (can do random number generator)
+    // after selecting enter atm pin 
+    // promt -> enter atm pin;
+    // after entering correct pin 
+    // a; change pin      b; fast cash
+    // c; fund transfer   d; withdrawal
+    // e; Mobile banking  f; balance enquiry
+    //              g; exit
+    // promt -> select account type
+    // a; savings 
+    // b; current 
+    // c; credit card
+
+    // promt -> enter amount (in the multiple of 100)
+    // allow upto 5 digit in amount in int format
+    // enter
+
+    // prompt -> please wait while your transaction is being processed 
+    // after 1 min 
+    // prompt  -> please collect your cash 
+    return 0;
 }
