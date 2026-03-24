0a3df9b4e2d1eaff1247aca7e17beb6d229a7638
GODARC0 <mohdluqmaanishere@gmail.com>
2026-03-24 23:24:19 +0530

    starting a new project

---
+++ C++/Robotics_geometry/geometry.cpp
@@ -0,0 +1,10 @@
+#include <iostream.h>
+using namespace std;
+
+int main(){
+    //in this project we will calculate distance between 2 points
+    //input will be in (x1,y1,x2,y2) coordinate and also angles
+
+    //how this will proceed we will soon see...
+    return 0;
+}
