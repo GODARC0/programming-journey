f2303f8032204296c6b257a83f9e2cb359954cd5
GODARC0 <mohdluqmaanishere@gmail.com>
2026-05-28 21:45:10 +0530

    adding objective

---
+++ C++/skillbits_projects/stud_gradebook.cpp
@@ -0,0 +1,3 @@
+// Build a program to manage student grades. Allow users
+// to input student information ,including names and scores for
+// multiple subjects, and calculate the average score of each student
