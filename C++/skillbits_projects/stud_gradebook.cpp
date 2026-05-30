09a6cd4e6d0cd010acddc7a194bf7f5a92772590
GODARC0 <mohdluqmaanishere@gmail.com>
2026-05-30 16:46:19 +0530

    started coding

--- C++/skillbits_projects/stud_gradebook.cpp
+++ C++/skillbits_projects/stud_gradebook.cpp
@@ -1,3 +1,11 @@
 // Build a program to manage student grades. Allow users
 // to input student information ,including names and scores for
 // multiple subjects, and calculate the average score of each student
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    
+    return 0;
+}
