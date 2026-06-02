c6b388aba1f41f01d1193175d924373f991a429b
GODARC0 <mohdluqmaanishere@gmail.com>
2026-06-02 22:59:38 +0530

    adding boiler code

--- C++/skillbits_projects/lib_manage_system.cpp
+++ C++/skillbits_projects/lib_manage_system.cpp
@@ -1,3 +1,12 @@
 // develop a console based library management system that 
 // allows users to add and remove books, check book availability 
 // , and manage borrower records.
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    //create a admim login 
+    return 0;
+}
