863eb7f7f4473fd0e4adeec821be2ed9a6e225e9
GODARC0 <mohdluqmaanishere@gmail.com>
2026-06-16 13:29:57 +0530

    starting new project

--- C++/skillbits_projects/contact_manage.cpp
+++ C++/skillbits_projects/contact_manage.cpp
@@ -1,2 +1,23 @@
 // create a program that manages a list of contacts. users
 // should be able to add, update, delete and view contact information.
+#include <iostream>
+#include <stdio.h>
+#include "sqlite3.h"
+using namespace std;
+
+int main(){
+    //for storing contacts i had 2 options one with stings of json code and other was sqlite
+    //im going to use sqlite because im more familiar with that 
+    sqlite3* DB;
+
+    int result = sqlite3_open("my_database.db", &DB);
+    if (result != SQLITE_OK) {
+        std::cerr << "Error opening database!" << std::endl;
+        return 1; 
+    }
+    std::cout << "Database opened/created successfully!" << std::endl;
+    sqlite3_close(DB);
+
+
+    return 0;
+}
