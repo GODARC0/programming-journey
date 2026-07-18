3829a17b8c2dad5fc38730f43b3c618429197d7f
GODARC0 <mohdluqmaanishere@gmail.com>
2026-07-18 20:22:25 +0530

    fixing bugs, improving logic and adding display

--- C++/skillbits_projects/stud_gradebook.cpp
+++ C++/skillbits_projects/stud_gradebook.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <vector>
 #include  <limits>
+#include <iomanip>
 using namespace std;
     
 struct Student {
@@ -20,17 +21,26 @@ struct Student {
     float avg;
     float percent;
 };
+
+char choice;
 int main(){
     //need a storage to maintain students grades
     //get various user inputs 
     vector<Student> students;
+    int idCounter = 0;
+    bool isFirstStudent = true;
 do{
     Student s1;
     std::cout << "Enter students full name: ";
-    
+    if (!isFirstStudent) {
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+    isFirstStudent = false;
     std::getline(cin, s1.fullName);
     cout << "Note : if the student had not opted that subject please fill N/A in place of marks\n";
     
+    
+    s1.uid = ++idCounter;
     int total = 0;
     int count = 0;
     std::string temp;
@@ -98,7 +108,7 @@ do{
         s1.english = -1;
     }
 
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
     // calculate and display average score of students
     s1.avg =  static_cast<float>(total)/count;
     s1.percent = (static_cast<float>(total)/(count*80))*100;
@@ -109,8 +119,18 @@ do{
     cout<<"percentage :"<<s1.percent<<"%\n";
     students.push_back(s1);
     
-    continue ;
+    
+    cout<<"Add another student? (y/n): "<<"\n";
+    cin>>choice;
+    
 }
-while(//ask after every loop "add student and continue if the input is Y and exit if input is N")
+while(choice == 'y');
+    cout << setw(5) << "uid" << setw(15) << "fullName" << setw(10) << "physics" <<setw(10)<< "chemistry" <<setw(10)<< "biology" <<setw(10)<< "maths" <<setw(10)<< "compSci" <<setw(10)<< "english" <<setw(10)<< "avg" << setw(10)<< "%" <<setw(10)<< "\n";
+for (Student s : students) {
+    cout << setw(5) << s.uid << setw(15) << s.fullName << setw(10) << s.physics <<setw(10)<< s.chemistry <<setw(10)<< s.biology <<setw(10)<< s.maths <<setw(10)<< s.compSci <<setw(10)<< s.english <<setw(10)<< s.avg << setw(10)<< s.percent <<setw(10)<< "\n";
+}
+
+
+
     return 0;
 }
